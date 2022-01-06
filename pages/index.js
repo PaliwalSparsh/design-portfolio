8c3e5d3c9eac1b9815392fb9f55095c36857e5bd
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2022-01-06 11:41:56 -0500

    Add homepage layout

--- pages/index.js
+++ pages/index.js
@@ -2,72 +2,246 @@ import Image from "next/image";
 
 function Container() {
   return (
-    <div className="grid grid-cols-4 gap-y-96 p-8">
-      <FirstContainer />
-      <SecondContainer />
-      <ThirdContainer />
-      <FourthContainer />
-      <FifthContainer />
+    <div>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
 
-function FirstContainer() {
+function Header() {
   return (
-    <div className="col-span-4">
-      <h1 className="text-6xl text-center font-bold text-gray-800">How Pandemic Changed Music?</h1>
-    </div>
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">Sparsh Paliwal</span>
+        </a>
+        <nav className="md:ml-0 md:mr-0 flex flex-wrap items-center text-base justify-center">
+          <a className="mx-5 hover:text-gray-900">Work</a>
+          <a className="mx-5 hover:text-gray-900">Lab</a>
+          <a className="mx-5 hover:text-gray-900">About</a>
+        </nav>
+      </div>
+    </header>
   );
 }
 
-function SecondContainer() {
+function Footer() {
   return (
-    <div className="col-span-4">
-      <h1 className="text-6xl text-center font-bold text-gray-800">
-        How to find emotions programmatically?
-      </h1>
-    </div>
+    <footer className="text-gray-600 body-font">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <p className="text-sm md:text-lg text-gray-500 sm:py-2 sm:mt-0 mt-4">
+          © 2021 Sparsh Paliwal
+        </p>
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+          <a className="text-gray-500 hover:text-blue-500">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5 md:w-7 md:h-7"
+              viewBox="0 0 24 24"
+            >
+              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-500 hover:text-blue-500">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5 md:w-7 md:h-7"
+              viewBox="0 0 24 24"
+            >
+              <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-500 hover:text-amber-500">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-5 h-5 md:w-7 md:h-7"
+              viewBox="0 0 24 24"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+            </svg>
+          </a>
+          <a className="ml-3 text-gray-500 hover:text-blue-500">
+            <svg
+              fill="currentColor"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="0"
+              className="w-5 h-5 md:w-7 md:h-7"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="none"
+                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+              ></path>
+              <circle cx="4" cy="4" r="2" stroke="none"></circle>
+            </svg>
+          </a>
+        </span>
+      </div>
+    </footer>
   );
 }
 
-function ThirdContainer() {
+function Content() {
   return (
-    <div className="col-span-4">
-      <h1 className="text-6xl text-center font-bold text-gray-800">
-        Emotion of all songs from 2017 to 2021.
-      </h1>
-    </div>
-  );
-}
-
-function FourthContainer() {
-  return (
-    <div className="col-span-4">
-      <h1 className="text-6xl text-center font-bold text-gray-800">
-        Pre and post-pandemic emotions.
-      </h1>
-    </div>
-  );
-}
-
-function FifthContainer() {
-  return (
-    <div className="col-span-4">
-      <h1 className="text-6xl text-center font-bold text-gray-800">Conclusion</h1>
-    </div>
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+            I bring <span className="underline decoration-4 decoration-orange-500">design</span> and{" "}
+            <span className="underline decoration-4 decoration-blue-500">engineering</span> to the
+            table.
+          </h1>
+        </div>
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-full sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img
+                alt="gallery"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+                src="https://dummyimage.com/600x360"
+              />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                  THE SUBTITLE
+                </h2>
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                  Shooting Stars
+                </h1>
+                <p className="leading-relaxed">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing
+                  tousled waistcoat.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img
+                alt="gallery"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+                src="https://dummyimage.com/601x361"
+              />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                  THE SUBTITLE
+                </h2>
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
+                <p className="leading-relaxed">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing
+                  tousled waistcoat.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img
+                alt="gallery"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+                src="https://dummyimage.com/603x363"
+              />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                  THE SUBTITLE
+                </h2>
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                <p className="leading-relaxed">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing
+                  tousled waistcoat.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img
+                alt="gallery"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+                src="https://dummyimage.com/602x362"
+              />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                  THE SUBTITLE
+                </h2>
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
+                <p className="leading-relaxed">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing
+                  tousled waistcoat.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img
+                alt="gallery"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+                src="https://dummyimage.com/605x365"
+              />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                  THE SUBTITLE
+                </h2>
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                  Holden Caulfield
+                </h1>
+                <p className="leading-relaxed">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing
+                  tousled waistcoat.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <img
+                alt="gallery"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+                src="https://dummyimage.com/606x366"
+              />
+              <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+                <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+                  THE SUBTITLE
+                </h2>
+                <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
+                <p className="leading-relaxed">
+                  Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing
+                  tousled waistcoat.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
 export default Container;
-
-{
-  /* <div className="hidden max-w-md sm:max-w-full mt-16">
-  <div className="h-60 w-full sm:object-cover relative rounded-lg shadow-lg">
-    <Image alt="placeholder" layout="fill" src="https://via.placeholder.com/500" />
-  </div>
-  <p className="mt-8">
-    We often associate music with emotions. For example, Uptown Funk is a happy song, Scientist by
-    ColdPlay is a sad song.
-  </p>
-</div>; */
-}
