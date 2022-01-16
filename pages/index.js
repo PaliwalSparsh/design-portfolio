6c1737325ecfa3869755edd7454dd2397339f398
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2022-01-16 02:30:36 -0800

    Add all projects

--- pages/index.js
+++ pages/index.js
@@ -48,10 +48,48 @@ function Projects() {
       </h2>
       <div className="mt-6 flex flex-wrap flex-row">
         <Project
-          title="Sketchbook"
-          org="Pharmeasy"
+          title="FormA11y"
+          org="Adobe Research"
           orgColor="red-500"
-          year="2020"
+          year="WIP"
+          imageSrc="/sketchbook.png"
+          imageAlt="/sketchbook.png"
+          description="A tool to help designers and developers create and share interactive prototypes."
+        />
+        <Project
+          title="Game for Discovery of Preferences"
+          org="Google Summer of Code"
+          orgColor="amber-500"
+          year="2016"
+          imageSrc="/sketchbook.png"
+          imageAlt="/sketchbook.png"
+          description="A tool to help designers and developers create and share interactive prototypes."
+        />
+        <Project
+          title="Music Through the Pandemic"
+          org="Data Visualisation Course"
+          orgColor="purple-500"
+          year="2021"
+          imageSrc="/sketchbook.png"
+          imageAlt="/sketchbook.png"
+          description="A tool to help designers and developers create and share interactive prototypes."
+        />
+        <Project
+          title="Component Library"
+          org="Interaction Design Course"
+          orgColor="purple-500"
+          year="2016"
+          imageSrc="/sketchbook.png"
+          imageAlt="/sketchbook.png"
+          description="A tool to help designers and developers create and share interactive prototypes."
+        />
+        <Project
+          title="VR for Education"
+          org="Final Year Project"
+          orgColor="purple-500"
+          year="2016"
+          imageSrc="/sketchbook.png"
+          imageAlt="/sketchbook.png"
           description="A tool to help designers and developers create and share interactive prototypes."
         />
       </div>
@@ -61,13 +99,15 @@ function Projects() {
 
 function Project(props) {
   return (
-    <div className="md:w-1/3">
+    <div className="mt-16 w-full md:w-1/3">
       <div className="text-xl font-bold text-white">{props.title}</div>
       <div className="text-base">
         <span className={`text-${props.orgColor}`}>{props.org}</span>
         <span className="text-neutral-500">–{props.year}</span>
       </div>
-      <div className="mt-2 md:mt-4 bg-neutral-900 rounded-2xl w-80 h-80"></div>
+      <div className="mt-2 md:mt-4 bg-neutral-900 rounded-2xl md:w-80 h-80">
+        {/* <Image src={props.imageSrc} alt={props.imageAlt} width={240} height={240} /> */}
+      </div>
       <div className="mt-2 md:mt-4 text-base text-neutral-500 leading-5">{props.description}</div>
     </div>
   );
